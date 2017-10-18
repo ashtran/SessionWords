36e8d765bd7f8359c6cb5e0e033164fdf380e5f9
Your Name <your_name@example.com>
2017-10-18 16:25:04 -0700

    initial commit

---
+++ apps/session_words/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse, redirect
+from datetime import datetime
+
+def index(request):
+    # try:
+    #     request.session['addword']
+    # except KeyError:
+    #     request.session['addword']=[]
+    return render(request, 'session_words/index.html')
+
+def process(request):
+    if request.method == "POST":
+        request.session['addword']=request.POST['addword']
+        request.session['color']=request.POST['color']
+        request.session['big']=request.POST['big']
+        return redirect('/')
+    else:
+        return redirect('/')
+
+def clear(request):
+    print "testing here"
+    del request.session['addword']
+    return redirect('/')
